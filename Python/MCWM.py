66a87733d2fdcf0717a45f6bb7fe9591e1105772
Phillip Hagen <37852582+Hage1055@users.noreply.github.com>
2019-05-13 15:47:57 -0700

    Add files via upload

--- Python/MCWM.py
+++ Python/MCWM.py
@@ -7,18 +7,24 @@ import RPi.GPIO as GPIO
 import time
 from tkinter import *
 import tkinter.font
+import spidev
+import board
+import neopixel
 
 # --- Pin settings ---
 
 PWMPin = 12 #PWM Pin connected to ENA
 Motor1 = 16 #Grinder motor connected to input 1
 Motor2 = 18 #Grinder motor connected to input 2
+LEDPin = 10 #LED Strip
+spi = spidev.SpiDev()
 
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD) #This accesses GPIOs according to physical location
+#GPIO.setmode(GPIO.BOARD) #This accesses GPIOs according to physical location
 GPIO.setup(PWMPin, GPIO.OUT) #Sets pin to an output
 GPIO.setup(Motor1, GPIO.OUT) #Sets pin to an output
 GPIO.setup(Motor2, GPIO.OUT) #Sets pin to an output
+pixels = neopixel.NeoPixel(board.D10,34) #D10 represents the pin, 34 is total LEDs
 
 GPIO.output(PWMPin, GPIO.LOW) #Set pins to low on startup
 GPIO.output(Motor1, GPIO.LOW)
@@ -26,13 +32,28 @@ GPIO.output(Motor2, GPIO.LOW)
 
 PwmValue = GPIO.PWM(PWMPin, 2000) #Sets PWM frequency
 PwmValue.start(100) #Duty cycle of motor in percent
+spi.open(0,0)
+
+# --- IR Sensor Read ---
+
+def ReadChannel(channel):
+    val = spi.xfer2([1, (8+channel)<<4,0])
+    data = ((val[1]&3)<<8) + val[2]
+    return data
+
+v = (ReadChannel(0)/1023.0)*3.3
+#The constants here might need to be changed according to the data sheet on
+#the IR Sensor
+dist = 16.2537*v**4 - 129.893*v**3 + 382.268*v**2 - 512.611*v + 301.439
 
 # --- GUI settings ---
 
 Gui = Tk()
 Gui.title("Automated BioChar Injection System")
 Gui.config(background = "#00FFFF")
-Gui.minsize(800,300)
+Gui.minsize(800,600)
+Gui.maxsize(800,600)
+Gui.resizable(width=False, height=False)
 Font1 = tkinter.font.Font(family = 'Helvetica', size = 18, weight = 'bold')
 
 def MotorClockwise():
@@ -85,5 +106,25 @@ Text3.grid(row=2,columnspan=2)
 
 Scale1 = Scale(Gui, from_=0, to=100, orient = HORIZONTAL, resolution = 1, command = ChangePWM)
 Scale1.grid(row=2,column=2)
+
+# --- LED Control ---
+
+#These distances need to be better tested and adjusted accordingly
+
+if dist >= 200:
+    pixels.fill((0,255,0))
+    Text4 = Label(Gui,text='Hopper Status',font=Font1,bg='#00FF00',fg='#FFFFFF',padx=50,pady=50)
+    Text4.grid(row=3,columnspan=2)
+elif dist < 200 & dist >= 140:
+    pixel.fill((255,0,0))
+    Text4 = Label(Gui,text='Hopper Status',font=Font1,bg='#FF0000',fg='#FFFFFF',padx=50,pady=50)
+    Text4.grid(row=3,columnspan=2)
+else:
+    pixel.fill((255,0,0))
+    Text4 = Label(Gui,text='Refill Immediately',font=Font1,bg='#FF0000',fg='#FFFFFF',padx=50,pady=50)
+    Text4.grid(row=3,columnspan=2)
+    sleep(5)
+    pixel-fill((0,0,0))
+    sleep(5)
 
 Gui.mainloop()
